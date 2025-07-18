0e8f5a0376867b23d958018b8ac735460ccaaed3
charan1147 <charanc1147@gmail.com>
2025-07-18 13:14:02 +0530

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-hotel-bookings-5.onrender.com",
+  baseURL: "https://project-hotel-bookings-6.onrender.com",
   withCredentials: true,
 });
 
