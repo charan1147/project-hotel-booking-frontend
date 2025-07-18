3c76dfa2fe0260ab33c2859d7dd5b12feca49c63
charan1147 <charanc1147@gmail.com>
2025-07-18 14:12:11 +0530

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-hotel-bookings-7.onrender.com",
-  withCredentials: true,
+  baseURL: "https://project-hotel-bookings-8.onrender.com",
+ 
 });
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
